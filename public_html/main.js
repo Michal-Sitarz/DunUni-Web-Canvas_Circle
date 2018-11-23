4cc79f7c6e880c2bb58e3ca62dac9388aab1a4b6
Michal-Sitarz <me@michalsitarz.com>
2018-11-23 15:33:21 +0000

    working: draw a circle

--- public_html/main.js
+++ public_html/main.js
@@ -1,8 +1,18 @@
+let colours = ['orange','blue','red','yellow','green'];
+let numberOfCircleIcons = 0;
+
+function drawIconCircle(){
+    numberOfCircleIcons++;
+    let circleSize = 64;
+    
+    addNewCanvas();
+    drawCircle(circleSize);
+    
+}
 
 function addNewCanvas(){
     let newCanvas = document.createElement("canvas");
-    newCanvas.setAttribute('id','circle-canvas');
-
+    newCanvas.setAttribute('id','circleIcon'+numberOfCircleIcons);
     var canvasAltText = document.createTextNode("Circle using canvas tag");
     newCanvas.appendChild(canvasAltText);
 
@@ -10,27 +20,26 @@ function addNewCanvas(){
     mainDiv.appendChild(newCanvas);
 }
 
-function drawCircle(sizeOfCanvas){
-    addNewCanvas();
-    
-    let circleCanvas = document.getElementById('circle-canvas');
-    circleCanvas.width = sizeOfCanvas;
-    circleCanvas.height = sizeOfCanvas;
+function drawCircle(sizeOfCircle){
+        
+    let circleCanvas = document.getElementById('circleIcon'+numberOfCircleIcons);
+    circleCanvas.width = sizeOfCircle;
+    circleCanvas.height = sizeOfCircle;
     
     let ctx = circleCanvas.getContext('2d');
     
-    let circleX = sizeOfCanvas/2;
-    let circleY = sizeOfCanvas/2;
-    let circleSize = (sizeOfCanvas/2)-2;
+    let circleX = sizeOfCircle/2;
+    let circleY = sizeOfCircle/2;
+    let circleSize = (sizeOfCircle/2)-2;
     
     //default for full circle
     let circleStart = 0 * Math.PI;
     let circleEnd = 2 * Math.PI;
-    let drawClockwise = true;
+    let drawAntiClockwise = false;
+
     
     ctx.beginPath();
-    ctx.arc(circleX,circleY,circleSize,circleStart,circleEnd,drawClockwise);
-    
+    ctx.arc(circleX,circleY,circleSize,circleStart,circleEnd,drawAntiClockwise);
     // circle fill
     ctx.fillStyle = 'orange';
     ctx.fill();
@@ -42,6 +51,14 @@ function drawCircle(sizeOfCanvas){
     
 }
 
-drawCircle(64);
+drawIconCircle();
 
+/*
+function draw1000circles(){
+    for (i=0;i<1000;i++){
+        drawIconCircle();
+    }
+}
 
+draw1000circles();
+*/
